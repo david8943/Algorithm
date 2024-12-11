d307bdb684444c79bf3e28f15bba7c94a28a1184
david8943 <47552691+david8943@users.noreply.github.com>
2024-12-11 16:21:21 +0900

    [Silver III] Title: 프린터 큐, Time: 76 ms, Memory: 12040 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -7,49 +7,48 @@ public class Main {
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
-        for(int t=0;t<T;t++){
+        for(int t=0; t<T; t++){
             st = new StringTokenizer(br.readLine());
             // 문서의 개수 N과 몇 번째로 인쇄되었는지 궁금한 문서 M
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
             int result = 0;
-            Deque<Integer> deque = new ArrayDeque<>();
+            
+            // Deque에 (중요도, 인덱스) 형태로 저장
+            Deque<int[]> deque = new ArrayDeque<>();
             int[] check = new int[10]; // 1 이상 9 이하 정수
+            
             st = new StringTokenizer(br.readLine());
-            for(int i=0;i<n;i++) {
+            for(int i=0; i<n; i++) {
                 int temp = Integer.parseInt(st.nextToken());
-                deque.offer(temp);
+                deque.offer(new int[]{temp, i}); // 중요도와 인덱스를 함께 저장
                 check[temp]++;
             }
 
             while(true){
-                int temp_num = deque.pollFirst();
+                int[] current = deque.pollFirst(); // 현재 문서 (중요도, 인덱스)
+                int temp_num = current[0]; // 중요도
+                int index = current[1]; // 인덱스
                 boolean is_printed = true;
 
-                for(int i=temp_num+1;i<10;i++){
-                    if(check[i]!=0) {
+                // 현재 문서보다 높은 중요도가 있는지 확인
+                for(int i = temp_num + 1; i < 10; i++){
+                    if(check[i] != 0) {
                         // 인쇄하지 않고 큐 가장 뒤에 재배치
-                        deque.offerLast(temp_num);
+                        deque.offerLast(current);
                         is_printed = false;
                         break;
                     }
                 }
+
                 if(is_printed){
                     check[temp_num]--;
                     result++;
-//                    System.out.println("print "+temp_num);
-                    if(m==0){
+                    if(index == m){ // 찾고 있던 문서가 출력되었는지 확인
                         break;
                     }
                 }
-                // 출력이 안됐으면 m 인덱스 조정
-                if(m==0){
-                    m = deque.size()-1;
-                } else {
-                    m--;
-                }
             }
-//            System.out.println("result "+result);
             System.out.println(result);
         }
     }
