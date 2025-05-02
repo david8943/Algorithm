82275b2d797dc8bbf32b9fb87ece31791bfe6aad
david8943 <47552691+david8943@users.noreply.github.com>
2025-05-02 21:02:33 +0900

    [level 4] Title: 도둑질, Time: 30.29 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        int n = money.length;
+        
+        // 첫번째 집 털 때
+        int[] dp1 = new int[n];
+        dp1[0] = money[0];
+        dp1[1] = Math.max(money[0], money[1]);
+        
+        for(int i=2;i<n-1;i++) {
+            dp1[i] = Math.max(dp1[i-2] + money[i], dp1[i-1]);
+        }
+        dp1[n-1] = dp1[n-2];
+        
+        
+        // 첫번째 집 안털 때
+        int[] dp2 = new int[n];
+        dp2[1] = money[1];
+        
+        for(int i=2;i<n;i++) {
+            dp2[i] = Math.max(dp2[i-2] + money[i], dp2[i-1]);
+        }
+        answer = Math.max(dp1[n-1], dp2[n-1]);
+        
+        return answer;
+    }
+}
