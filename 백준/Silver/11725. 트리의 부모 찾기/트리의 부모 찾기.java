aa2a398b1fec2b34fa1f309374fe51db9e7c48ea
david8943 <47552691+david8943@users.noreply.github.com>
2025-06-13 16:01:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 532 ms, Memory: 65232 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,19 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
-    static int n;
-    static boolean[] v;
-    static ArrayList<Integer> list[];
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        parent = new int[n+1];
-        list = new ArrayList[n+1];
-        v = new boolean[n+1];
+        int[] parent = new int[n+1];
+        ArrayList<Integer> list[] = new ArrayList[n+1];
+        boolean[] v = new boolean[n+1];
 
         for(int i=1;i<=n;i++) {
             list[i] = new ArrayList<>();
@@ -29,20 +25,24 @@ public class Main {
             list[b].add(a);
         }
 
-        dfs(1);
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        v[1] = true;
+        q.offer(1);
 
-        for( int i=2;i<=n;i++) {
-            System.out.println(parent[i]);
-        }
-    }
-
-    static void dfs(int idx) {
-        v[idx] = true;
-        for(int i:list[idx]) {
-            if(!v[i]) {
-                parent[i] = idx;
-                dfs(i);
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for(int next : list[cur]) {
+                if(!v[next]) {
+                    v[next] = true;
+                    parent[next] = cur;
+                    q.offer(next);
+                }
             }
         }
+
+        for( int i=2;i<=n;i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
