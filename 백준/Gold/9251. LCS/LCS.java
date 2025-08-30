f0223fb21f0733f5517b2441dcc1b6fc59921882
david8943 <47552691+david8943@users.noreply.github.com>
2025-08-30 22:28:49 +0900

    [Gold V] Title: LCS, Time: 96 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int[][] dp = new int[a.length()+1][b.length()+1];
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if(a.charAt(i) == b.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                } else {
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+
+        System.out.println(dp[a.length()][b.length()]);
+    }
+}
